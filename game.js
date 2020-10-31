91b0fa254a17bc90eee8934a787c1431d6e1cee5
Gunjan Bangde <bangdegunjan@gmail.com>
2020-11-01 01:04:07 +0530

    simon game complete

---
+++ game.js
@@ -0,0 +1,70 @@
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var first = true;
+var level = 0;
+
+$(document).keypress(function () {
+    if (first) {
+        nextSequence();
+        first = false;
+    }
+})
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        var wroungAudio = new Audio("sounds/wrong.mp3");
+        wroungAudio.play();
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        console.log("wrong");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function startOver(){
+    level=0;
+    first=true;
+    gamePattern=[];
+}
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    playSound(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+$(".btn").click(function () {
+    var userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1)
+})
